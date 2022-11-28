a4d7f5ef11a94d72bb88f602304ad1e6f4af7486
Souuum <hugo.soumbrugevin@gmail.com>
2022-11-28 11:44:12 +0100

    Started Quizz Ui and data fetching

--- src/screens/Quizz.js
+++ src/screens/Quizz.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useContext } from "react";
+import { useEffect, useState, useCallback, useContext } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Image, Text } from "react-native";
 import FredokaText from "../components/FredokaText";
@@ -10,13 +10,50 @@ import Title from "../components/Title";
 import NavLink from "../components/NavLink";
 import SafeContainer from "../components/SafeContainer";
 import { RED, ICON, STYLE } from "../components/config.js";
+import { host } from "../config/host"
+import { useAuth } from "../contexts/Auth";
 
 const Quizz = ({ navigation }) => {
+    const { authData } = useAuth();
+    const [questions, setQuestion] = useState(null);
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                await fetch(`http://${host}:3000/users_questions_history/user/${authData.id}/not-answered`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                }).then((response) => response.json())
+                    .then(json => {
+                        if (json === null || typeof (json) == undefined || json.length == 0) {
+                            authData.questions = null;
+                        } else {
+                            console.log(json);
+                            console.log(json[0])
+                            authData.questions = json;
+                        }
+                    });
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+        prepare();
+
+        return () => {
+
+        }
+    }, [])
+
+
     return (
         <SafeContainer>
             <Title fontSize={"25px"} additionnalStyle={{ marginTop: 30 }}>
-                {"Inscription"}
+                {"Quizz"}
             </Title>
         </SafeContainer>
     )
 }
+
+export default Quizz;
