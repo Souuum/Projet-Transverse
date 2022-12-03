b6ea32b8722e727968616869ff9f3b605b8996fb
Souuum <hugo.soumbrugevin@gmail.com>
2022-12-03 13:09:10 +0100

    quizzswiper

--- src/components/QuizzSwiper.js
+++ src/components/QuizzSwiper.js
@@ -9,8 +9,8 @@ import FredokaText from "./FredokaText.js";
 const CardContainer = styled.View`
 background-color: ${(props) => props.bgColor};
 padding: 15px 25px;
-width: 30%;
-height: 60%;
+width: 80%;
+height: 50%;
 border-radius: 20px;
 display: flex;
 flexDirection: column;
@@ -18,10 +18,19 @@ justifyContent: space-between;
 
 `;
 
+const SwipeContainer = styled.View`
+background-color: ${(props) => props.bgColor};
+
+width: 82%;
+height: 60%;
+border-radius: 20px;
+
+`;
+
 
 
 const Card = (props) => {
-    console.log(props.card);
+    // console.log(props.card);
     return (
         <CardContainer bgColor={TRIADIC}>
             <FredokaText>{props.card.description}</FredokaText>
@@ -29,25 +38,34 @@ const Card = (props) => {
 
 };
 
-const QuizzSwiper = (data) => {
-    const d = [{ "id": 3, "category": "Data", "description": "Les stats t'intéresse", "poids": 12 }, { "id": 1, "category": "Système embarqué", "description": "Tu aimes programmer sur des micros-controlleurs", "poids": 10 }, { "id": 2, "category": "IT", "description": "Tu t'intéresse au côté graphique des interfaces", "poids": 10 }];
-    console.log(data.cards);
+
+
+const QuizzSwiper = (ref, data) => {
+
     const _data = Object.keys(data.cards).map(key => ({ [key]: data.cards[key] }));
-    console.log(_data[0].cards);
+    // console.log(_data[0].cards);
     const [index, setIndex] = useState(0);
     const onSwiped = () => {
         setIndex(index + 1);
     };
 
     return (
-        <Swiper
-            cards={_data[0].cards}
-            cardIndex={index}
-            renderCard={(card) =>
-                <Card card={card}></Card>
-            }
-            onSwiper={onSwiped}
-        />
+        <SwipeContainer>
+            <Swiper
+                ref={ref}
+                backgroundColor={'transparent'}
+                cards={_data[0].cards}
+                cardIndex={index}
+                renderCard={(card) =>
+                    <Card card={card}></Card>
+                }
+                infinite
+                stackSize={3}
+                stackSeparation={14}
+                onSwiper={onSwiped}
+            />
+        </SwipeContainer>
+
     )
 }
 
